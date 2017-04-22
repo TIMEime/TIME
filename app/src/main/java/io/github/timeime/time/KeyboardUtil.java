176090dd509e8394b315144100ac89c2c14a0458
williamchi1996 <williamchi1996@gmail.com>
2017-04-22 14:28:18 +0800

    123

--- app/src/main/java/io/github/timeime/time/KeyboardUtil.java
+++ app/src/main/java/io/github/timeime/time/KeyboardUtil.java
@@ -8,7 +8,7 @@ public class KeyboardUtil {
     private KeyboardView keyboardView;
     private ImeService imeService;
     private Keyboard keyboardSmallLetter, keyboardNumber, keyboardCapitalLetter;
-
+    MainActivity llloo;
     public KeyboardUtil(ImeService imeService1, KeyboardView keyboardView1) {
         super();
         keyboardView = keyboardView1;
@@ -56,6 +56,7 @@ public class KeyboardUtil {
         public void onKey(int primaryCode, int[] keyCodes) {
             switch (primaryCode) {
                 case Keyboard.KEYCODE_SHIFT:
+                    llloo.builder.show();
                     if(keyboardView.getKeyboard() == keyboardCapitalLetter) {
                         keyboardView.setKeyboard(keyboardSmallLetter);
                     }else {
--- app/src/main/java/io/github/timeime/time/MainActivity.java
+++ app/src/main/java/io/github/timeime/time/MainActivity.java
@@ -1,13 +1,30 @@
 package io.github.timeime.time;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-}
+
+
+
+        public class MainActivity extends AppCompatActivity {
+        }
+            AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+            @Override
+            protected void onCreate(Bundle savedInstanceState) {
+                super.onCreate(savedInstanceState);
+                setContentView(R.layout.activity_main);
+                	//mainactivity是class name
+                builder.setTitle("this is標題");
+                builder.setMessage("here are內容");
+                builder.show();
+
+
+        }
+
+
+
+
+
+
