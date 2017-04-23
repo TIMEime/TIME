880ba98e1d26d1536b7e918ece910b369737780a
bear <qsc5367899@gmail.com>
2017-04-23 23:30:22 +0800

    資料庫可存名稱+內容，imageButton

--- app/src/main/java/io/github/timeime/time/MainActivity.java
+++ app/src/main/java/io/github/timeime/time/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -12,8 +13,8 @@ import java.util.ArrayList;
 public class MainActivity extends Activity {
     private EditText editText;
     private EditText editText2;
-    private Button addButton;
-    private Button deleteButton;
+    private ImageButton addButton;
+    private ImageButton deleteButton;
     private ListView listView;
     private ArrayList<DB> mDBs = new ArrayList<DB>();
     public  DBHelper dbHelper = new DBHelper(this, null, null, 1);
@@ -22,8 +23,8 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        addButton=(Button)findViewById (R.id.add_button);
-        deleteButton=(Button)findViewById (R.id.delete_button);
+        addButton=(ImageButton)findViewById (R.id.button);
+        deleteButton=(ImageButton)findViewById (R.id.button2);
         editText=(EditText)findViewById (R.id.edit_text);
         editText2=(EditText)findViewById (R.id.edit_text2);
         listView = (ListView) findViewById (R.id.main_list);
